1c0b54cd89c9e5f22da22b56e52d27cda026204d
6110110628 <6110110628@psu.ac.th>
2021-06-23 19:23:19 +0700

    new

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //General
+  static String appName = 'Shopping Mall';
+
+  //Route
+  static String routeAuthen = '';
+  static String routeCreateAccount = '';
+  static String routeBuyerService = '';
+  static String routeSalerService = '';
+  static String routeRiderService = '';
+}
